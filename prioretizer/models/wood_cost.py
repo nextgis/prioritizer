9ff48ec93ca530f380273c56024be8e020e482f2
Dmitry Kolesov <kolesov.dm@gmail.com>
2017-07-21 13:06:35 +0300

    Refactor: use wood cost module, but not script

---
+++ prioretizer/models/wood_cost.py
@@ -0,0 +1,65 @@
+# encoding: utf-8
+
+import uuid
+
+from collections import namedtuple
+
+from utils import temp_name
+
+
+Wood_costs = namedtuple('WoodCost', 'label, wood_cost, d_cost, h_cost, b_cost')
+
+
+class WoodCost:
+    def __init__(self,
+                 grass,
+                 mapname,
+                 forest_type_column, forest_count_column,
+                 diameter_column, height_column,
+                 perspective_column, bonitet_column,
+                 cumulative_cost
+    ):
+        """Create raster of walking cost using costs of particular rasters
+
+        :param grass:   Initialized GIS engine to operate raster data
+        :type grass:    prioretizer.grasslib.grasslib.GRASS
+        """
+        self.grs = grass
+
+        self.mapname = mapname
+
+        self.forest_type_column = forest_type_column
+        self.forest_count_column = forest_count_column
+        self.diameter_column = diameter_column
+        self.height_column = height_column
+        self.perspective_column = perspective_column
+        self.bonitet_column = bonitet_column
+
+        self.cumulative_cost = cumulative_cost
+
+    def wood_cost(self, output, cost_params, persp_factor, background_cost, overwrite=False):
+
+        # Create column with cost, then rasterize the column.
+        # It should be faster and simple then raster calculations (less rasterisation)
+
+        for wc in cost_params:
+            expression = "(%(count_col)s*%(wood_cost)s + %(bon_cost)s/%(bonitet)s + %(d_cost)s*%(diam)s + %(h_cost)s*%(h)s)*(%(persp_fact)s*%(persp)s+1)" \
+                % dict(
+                    count_col=self.forest_count_column,
+                    wood_cost=wc.wood_cost,
+                    bon_cost=wc.b_cost,
+                    bonitet=self.bonitet_column,
+                    diam=self.diameter_column,
+                    d_cost=wc.d_cost,
+                    h=self.height_column,
+                    h_cost=wc.h_cost,
+                    persp=self.perspective_column,
+                    persp_fact=persp_factor,
+            )
+            where = "%(type_col)s = \"%(label)s\"" % dict(type_col=self.forest_type_column, label=wc.label)
+            self.grs.grass.run_command('v.db.update', map=self.mapname, column=self.cumulative_cost, value=expression, where=where)
+
+        self.grs.grass.run_command('v.to.rast', input=self.mapname, output=output, use='attr', attribute_column=self.cumulative_cost, overwrite=overwrite)
+
+        expression = '{result} = if(isnull({result}), {background}, {result})'.format(result=output, background=background_cost)
+        self.grs.grass.run_command('r.mapcalc', expression=expression, overwrite=True)
