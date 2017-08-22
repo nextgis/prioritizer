28b6bff22ccc2504af5f0b59041ee4d81c503c44
Dmitry Kolesov <kolesov.dm@gmail.com>
2017-08-22 17:32:42 +0300

    Update docs

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='prioretizer',
-      version='1.0.0',
+      version='1.0.1',
       description='Prioretization',
 
       url='https://github.com/nextgis/prioritizer',
